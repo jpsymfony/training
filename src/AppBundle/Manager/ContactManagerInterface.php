1c7f63bc61c2b56d791df940097ce3ff8475aec0
jpSymfony <jpsymfony@free.fr>
2017-06-26 20:04:33 +0200

    fix

--- src/AppBundle/Manager/ContactManagerInterface.php
+++ src/AppBundle/Manager/ContactManagerInterface.php
@@ -2,8 +2,7 @@
 
 namespace AppBundle\Manager;
 
-use AppBundle\Entity\Contact;
-use AppBundle\Repository\ContactRepositoryInterface
+use AppBundle\Repository\ContactRepositoryInterface;
 
 interface ContactManagerInterface extends ContactRepositoryInterface
 {
